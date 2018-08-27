aa9605a37d242e8765693730debeabec1a8339f2
AzharLinux123 <39721058+AzharLinux123@users.noreply.github.com>
2018-08-26 21:09:09 -0300

    Create API.php

---
+++ API.php
@@ -0,0 +1,12 @@
+<?php
+
+//Regards
+date_default_timezone_set('Asia/Jakarta');
+$date = date('F d, Y, h:i A T');
+
+/* API SETUP */
+$smtp_acc = [
+    [
+        "token" => "1234hvhv",
+    ],
+];
